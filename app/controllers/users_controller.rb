50614863d1d90f045fc52c1a9243f23428f33be1
Mark Holmberg <mark.d.holmberg@gmail.com>
2012-03-20 12:19:55 -0600

    Intermediate commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,45 @@
 class UsersController < ApplicationController
 #before_filter :authorize
-  load_and_authorize_resource
+  #load_and_authorize_resource
+  authorize_resource
   #SPEC: 2.1 Student Column
   #SPEC: 2.1.1 List all students(users)
   def index
     #SPEC: 3.1.1 List all students(users)
-    authorize! :list, :students
     if params[:roles_mask] == '1'
-      @users = User.students
+      if params[:sort] == "name"
+        if params[:asc] == '1'
+          @users = User.students.order("name ASC")
+          authorize! :list, :students
+        elsif params[:asc] == '0'
+          @users = User.students.order("name DESC")
+          authorize! :list, :students
+        else
+          @users = User.students
+          authorize! :list, :students
+        end
+      else
+        @users = User.students
+        authorize! :list, :students
+      end #end sort by name
     elsif params[:roles_mask] == '2'
-      authorize! :list, :advisors
       @users = User.advisors
+      authorize! :list, :advisors
     elsif params[:roles_mask] == '4'
-      authorize! :list, :professors
       @users = User.professors
+      authorize! :list, :professors
     elsif params[:roles_mask] == '8'
-      authorize! :list, :gods
       @users = User.gods
+      authorize! :list, :gods
     else
+      @users = User.where("roles_mask < 4")
       authorize! :manage, User
     end
   end
 
   def show
-    authorize! :read, User
+    @user = User.find(params[:id])
+    authorize! :manage, User
   end
 
   #SPEC: 2.1.3 Add a new User(student)
@@ -35,9 +51,13 @@ class UsersController < ApplicationController
   #SPEC: 2.1.5 Edit a users information
   def edit
     #TODO: make sure they can only edit THEIR login if they're a student
+    @user = User.find(params[:id])
+    authorize! :manage, User
   end
 
   def create
+    @user = User.new(params[:user])
+    authorize! :create, User
     if @user.save
       redirect_to @user, notice: 'User was successfully created.'
     else
@@ -48,10 +68,12 @@ class UsersController < ApplicationController
   #SPEC: 1.1.3.2. Edit Login
   def edit_password
     @user = User.find_by_id(current_user.id)
+    authorize! :edit_password, User
   end
 
   def update_password
     @user = User.find_by_id(current_user.id)
+    authorize! :update_password, User
     if @user.authenticate(params[:user][:current_password])
       if @user.update_attributes( :password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation] )
         redirect_to @user, notice: "Password was updated!"
@@ -68,10 +90,12 @@ class UsersController < ApplicationController
   #SPEC: 1.1.3.2.1 Edit Only Their Own Details
   def edit_my_details
     @user = User.find_by_id(current_user.id)
+    authorize! :edit_my_details, User
   end
 
   def update_my_details
     @user = User.find_by_id(current_user.id)
+    authorize! :update_my_details, User
     if @user.update_attributes(params[:user])
       redirect_to @user, notice: 'Your information has been updated.'
     else
@@ -82,6 +106,8 @@ class UsersController < ApplicationController
   #SPEC: 3.1.3 Update Student Status
   def update
     authorize! :assign_roles, @user if params[:user][:assign_roles]
+    @user = User.find(params[:id])
+    authorize! :manage, User
     if @user.update_attributes(params[:user])
       redirect_to @user, notice: 'User was successfully updated.'
     else
@@ -91,6 +117,8 @@ class UsersController < ApplicationController
 
   #SPEC: 2.1.4 Delete a User(student)
   def destroy
+    @user = User.find(params[:id])
+    authorize! :destroy, User
     @user.destroy
     redirect_to users_url
   end
