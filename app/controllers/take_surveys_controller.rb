3907b5a52ceb6e9dcc53ed47dfe66f010244821e
Mark Holmberg <mark.d.holmberg@gmail.com>
2012-03-25 21:13:37 -0600

    Intermediate commit.

--- app/controllers/take_surveys_controller.rb
+++ app/controllers/take_surveys_controller.rb
@@ -4,6 +4,16 @@ class TakeSurveysController < ApplicationController
   end
 
   def create
+    params.each do |key, value|
+      if key =~ /^response\d+$/
+        current_user.responses.new(:answer_id => value, :user_id => current_user.id)
+      end
+    end
+
+    current_user.save
+    if current_user.responses.any? { |response| response.errors.any? }
+      flash.now[:error] = current_user.responses.reduce("") { |memo, r| memo << r.errors.full_messages.join(", ") }
+    end
   end
 
   def show
--- app/models/response.rb
+++ app/models/response.rb
@@ -16,4 +16,22 @@ class Response < ActiveRecord::Base
   #SPEC 5.5.1.2 User ID's
   belongs_to :answer
   belongs_to :user
+
+  # A user can only have one response per question per survey
+  validate :has_unique_repsonse
+  validate :has_one_answer_per_question
+
+  private
+
+  def has_unique_repsonse
+    if (Response::find_all_by_answer_id_and_user_id( answer.id, user.id ).count > 0)
+      errors.add(:response, "You cannot answer the same question with the same answer!")
+    end
+  end
+
+  def has_one_answer_per_question
+    if (user.responses.joins(:answer).group(:question_id).where( :id => id ).count.first.try(:last) || 0 > 0)
+      errors.add(:response, "You cannot answer the same question with the multiple answers!")
+    end
+  end
 end
